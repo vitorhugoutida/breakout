670fec4b66db4a7531354039bcc17d110a4abcbd
vitorhugoutida <vitorhugoutida@gmail.com>
2024-05-27 17:24:36 -0300

    feat: colocando pontos no jogo

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Actor, CollisionType, Color, Engine, vec } from "excalibur"
+import { Actor, CollisionType, Color, Engine, Text, ExitViewPortEvent, vec, Font } from "excalibur"
 
 // 1 - Criar uma instancia de Engine, que respresenta o jogo
 
@@ -94,7 +94,7 @@ const yoffest = 20
 const colunas = 5
 const linhas = 3
 
-const corBloco = [Color.Violet, Color.Orange, Color.Yellow]
+const corBloco = [Color.Red, Color.Orange, Color.Yellow]
 
 const larguraBloco = (game.drawWidth / colunas) - padding - (padding / colunas)
 
@@ -104,6 +104,100 @@ const alturaBloco = 30
 
 const listaBlocos: Actor[] = []
 
+//  Renderizacao dos bloquinhos
+
+// Renderiza 3 linhas 
+for (let j = 0; j < linhas; j++) {
+
+	// renderiza o 5 bloquinhos 
+
+for (let i = 0; i < colunas; i++) {
+	listaBlocos.push(
+		new Actor({
+			x: xoffset + i * (larguraBloco + padding) + padding,
+			y: yoffest + j * (alturaBloco + padding) + padding,
+			width: larguraBloco,
+			height: alturaBloco,
+			color: corBloco[j]
+
+		})
+	)
+}
+}
+
+listaBlocos.forEach( bloco => {
+	// Define o tipo de colisor de cada bloco
+	bloco.body.collisionType = CollisionType.Active
+
+	// Adiciona cada bloco no game 
+	game.add(bloco)
+})
+
+// Adicionando pontuacao
+
+let pontos = 0
+
+const textoPontos = new Text({
+	text: "Hello World",
+	font: new Font({size: 20})
+})
+
+
+const objetoTexto = new Actor ({
+	x: game.drawWidth - 80,
+	y: game.drawHeight - 15
+})
+
+objetoTexto.graphics.use(textoPontos)
+
+game.add(objetoTexto)
+
+
+
+let colidindo: boolean = false
+
+bolinha.on("collisionstart", (event) => {
+
+	// Se o elemento colidido for um bloco da lista de blocos (destrutivel)
+
+	if (listaBlocos.includes(event.other) ) {
+		// Destruir o bloco colidido 
+		event.other.kill()
+	}
+
+	// rebater a bolinha - inventer as direcoes
+	//  "minimum translation vector" is a vector 'normalize()'
+
+	let interseccao = event.contact.mtv.normalize()
+
+	if (!colidindo) {
+		colidindo = true
+
+		//  interseccao.x e interssecao.y
+		//  O maior representa o eixo onde houve o contato
+
+		if ( Math.abs(interseccao.x) > Math.abs(interseccao.y) )  {
+			// bolinha.vel.x = -bolinha.vel.x
+			// bolinha.vel.x *= -1
+
+			bolinha.vel.x = bolinha.vel.x * -1 
+		} else {
+			// bolinha.vel.y = -bolinha.vel.y
+			// bolinha.vel.y *= -1
+			bolinha.vel.y = bolinha.vel.y * -1
+		}
+
+	}
+})
+
+bolinha.on("collisionend", () => {
+	colidindo = false
+})
+
+// bolinha.on("exitviewport", () => {
+// 	alert("E morreu")
+// 	window.location.reload()
+// })
 // Inicia o game
 
 game.start()
